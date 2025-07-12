4929ad98305b1f715712102429241dddd106a3b7
gauravvgithub <gauravraj7011@gmail.com>
2025-07-12 18:07:11 +0530

    footer major changes

--- includes/footer.php
+++ includes/footer.php
@@ -5,8 +5,8 @@
                 <div class="footer-top">
                     <div class="footer-left">
                         <div class="logo-footer">
-                            <a href="/" class="logo">
-                                <img src="image/logo/logo.png" class="rounded d-block img-fluid" style="width: 30%;"
+                            <a href="/" class="logo" style="display: flex; align-items: center; justify-content: center;">
+                                <img src="image/logo/logo.png" class="rounded d-block img-fluid" style="width: 40%;"
                                     alt="">
                             </a>
                         </div>
@@ -17,86 +17,112 @@
                         </div>
                         <div class="contact-footer">
                             <div class="address contact-top contact-footer-content">
-                                <p class="caption-2 text-body" style="font-size:14px">
+                                <p class="caption-2 text-body" style="font-size:18px; font-weight: bold;">
                                     Our address
                                 </p>
-                                <a href="#" class="text-body" style="font-weight: bolder;">CS-42, 4th floor, Ansal Plaza Mall, Vaishali, Ghaziabad-201010</a>
+
+                                <a href="#" class="text-body" style="font-weight: normal;">
+                                    <div>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+                                            <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
+                                            <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+                                        </svg>
+                                        CS-42, 4th floor, Ansal Plaza Mall,
+                                        <div style="margin-left: 24px !important;">
+                                            Vaishali, Ghaziabad-201010
+                                        </div>
+                                    </div>
+                                </a>
                             </div>
                             <div class="contact-bottom">
                                 <div class="contact-footer-content">
-                                    <p class="caption-2 text-body" style="font-size:14px">
+                                    <p class="caption-2 text-body" style="font-size:18px; font-weight: bold;">
                                         Support 24/7
                                     </p>
-                                    <a href="#" class="text-body" style="font-weight: bolder;">+91-9971645151</a>
+                                    <a href="#" class="text-body" style="font-weight: normal;"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-headset" viewBox="0 0 16 16">
+                                            <path d="M8 1a5 5 0 0 0-5 5v1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V6a6 6 0 1 1 12 0v6a2.5 2.5 0 0 1-2.5 2.5H9.366a1 1 0 0 1-.866.5h-1a1 1 0 1 1 0-2h1a1 1 0 0 1 .866.5H11.5A1.5 1.5 0 0 0 13 12h-1a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h1V6a5 5 0 0 0-5-5" />
+                                        </svg>&nbsp;&nbsp; 91-9971645151</a>
                                 </div>
                                 <div class="contact-footer-content">
-                                    <p class="caption-2 text-body" style="font-size:14px">
+                                    <p class="caption-2 text-body" style="font-size:18px; font-weight: bold;">
                                         Email Address
                                     </p>
-                                    <a href="#" class="text-body" style="font-weight: bolder;">contact@panapadvisors.com</a>
+                                    <a href="#" class="text-body" style="font-weight: normal;"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+                                            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
+                                        </svg>&nbsp;&nbsp; contact@panapadvisors.com</a>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="footer-center">
                         <div class="footer-content our-services footer-col-block">
-                            <div class="title-mobile label text-btn-uppercase text-body">
+                            <div class="title-mobile label text-btn-uppercase text-body" style="font-size: 18px !important; font-weight: bold !important;">
                                 Categories
                                 <i class="icon-arrow-51"></i>
                             </div>
                             <div class="tf-collapse-content" style="display: unset;">
                                 <ul>
                                     <li class="support-item-footer caption-1"><a href="our-services-1.php"
-                                            class="text-body">Fund Raising</a></li>
+                                            class="text-body" style="font-size: 16px !important; font-weight: normal !important;">
+                                            <img style="width: 30px; height: 30px; color:black" src="../image/icon/logo/04.png" alt="fund raising">
+                                            Fund Raising</a></li>
                                     <li class="support-item-footer caption-1"><a href="tailored-strategies.php"
-                                            class="text-body">Tailored Strategies
+                                            class="text-body" style="font-size: 16px !important; font-weight: normal !important;">
+                                            <img style="width: 30px; height: 30px; color:black" src="../image/icon/logo/03.png" alt="tailored strategies">
+                                            Tailored Strategies
                                         </a></li>
                                     <li class="support-item-footer caption-1"><a href="/financial-modelling.php"
-                                            class="text-body">Financial Modelling </a></li>
+                                            class="text-body" style="font-size: 16px !important; font-weight: normal !important;">
+                                            <img style="width: 30px; height: 30px; color:black" src="../image/icon/logo/02.png" alt="financial modelling">
+                                            Financial Modelling
+                                        </a></li>
                                     <li class="support-item-footer caption-1"><a href="/Investor-pitch-desck-design.php"
-                                            class="text-body">Deck Design</a></li>
+                                            class="text-body" style="font-size: 16px !important; font-weight: normal !important;">
+                                            <img style="width: 30px; height: 30px; color:black" src="../image/icon/logo/01.png" alt="deck design">
+                                            Deck Design
+                                        </a></li>
 
                                 </ul>
                             </div>
                         </div>
                         <div class="footer-content our-services footer-col-block">
-                            <div class="title-mobile label text-btn-uppercase text-body">
+                            <div class="title-mobile label text-btn-uppercase text-body" style="font-size: 16px !important; font-weight: bold !important;">
                                 Quick Links
                                 <i class="icon-arrow-51"></i>
                             </div>
                             <div class="tf-collapse-content" style="display: unset;">
                                 <ul>
-                                    <li class="support-item-footer caption-1"><a href="about-us.php"
-                                            class="text-body"><svg xmlns="http://www.w3.org/2000/svg" width="16"
-                                                height="16" fill="currentColor" class="bi bi-person"
+                                    <li class="support-item-footer caption-1" style="font-size: 16px !important; font-weight: normal !important;"><a href="about-us.php"
+                                            class="text-body"><svg xmlns="http://www.w3.org/2000/svg" width="22"
+                                                height="22" fill="currentColor" class="bi bi-person"
                                                 viewBox="0 0 16 16">
                                                 <path
                                                     d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
-                                            </svg>About Us</a></li>
-                                    <li class="support-item-footer caption-1"><a href="our-services-1.php"
-                                            class="text-body"><svg xmlns="http://www.w3.org/2000/svg" width="16"
-                                                height="16" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
+                                            </svg>&nbsp; About Us</a></li>
+                                    <li class="support-item-footer caption-1" style="font-size: 16px !important; font-weight: normal !important;"><a href="our-services-1.php"
+                                            class="text-body"><svg xmlns="http://www.w3.org/2000/svg" width="22"
+                                                height="22" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
                                                 <path
                                                     d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492M5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0" />
                                                 <path
                                                     d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115z" />
                                             </svg>
 
-                                            Services
+                                            &nbsp;Services
                                         </a>
                                     </li>
-                                    <li class="support-item-footer caption-1"><a href="our-teams-industry.php"
-                                            class="text-body"> <svg xmlns="http://www.w3.org/2000/svg" width="16"
-                                                height="16" fill="currentColor" class="bi bi-people-fill"
+                                    <li class="support-item-footer caption-1" style="font-size: 16px !important; font-weight: normal !important;"><a href="our-teams-industry.php"
+                                            class="text-body"> <svg xmlns="http://www.w3.org/2000/svg" width="22"
+                                                height="22" fill="currentColor" class="bi bi-people-fill"
                                                 viewBox="0 0 16 16">
                                                 <path
                                                     d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5" />
                                             </svg>
 
-                                            Team</a></li>
-                                    <li class="support-item-footer caption-1"><a href="our-expert.php"
-                                            class="text-body"><svg xmlns="http://www.w3.org/2000/svg" width="16"
-                                                height="16" fill="currentColor" class="bi bi-person-check"
+                                            &nbsp;Team</a></li>
+                                    <li class="support-item-footer caption-1" style="font-size: 16px !important; font-weight: normal !important;"><a href="our-expert.php"
+                                            class="text-body"><svg xmlns="http://www.w3.org/2000/svg" width="22"
+                                                height="22" fill="currentColor" class="bi bi-person-check"
                                                 viewBox="0 0 16 16">
                                                 <path
                                                     d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m1.679-4.493-1.335 2.226a.75.75 0 0 1-1.174.144l-.774-.773a.5.5 0 0 1 .708-.708l.547.548 1.17-1.951a.5.5 0 1 1 .858.514M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4" />
@@ -104,10 +130,10 @@
                                                     d="M8.256 14a4.5 4.5 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10q.39 0 .74.025c.226-.341.496-.65.804-.918Q8.844 9.002 8 9c-5 0-6 3-6 4s1 1 1 1z" />
                                             </svg>
 
-                                            Our Experts</a>
+                                            &nbsp;Our Experts</a>
                                     </li>
-                                    <li class="support-item-footer caption-1"><a href="faq.php" class="text-body">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                    <li class="support-item-footer caption-1" style="font-size: 16px !important; font-weight: normal !important;"><a href="faq.php" class="text-body">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22"
                                                 fill="currentColor" class="bi bi-patch-question" viewBox="0 0 16 16">
                                                 <path
                                                     d="M8.05 9.6c.336 0 .504-.24.554-.627.04-.534.198-.815.847-1.26.673-.475 1.049-1.09 1.049-1.986 0-1.325-.92-2.227-2.262-2.227-1.02 0-1.792.492-2.1 1.29A1.7 1.7 0 0 0 6 5.48c0 .393.203.64.545.64.272 0 .455-.147.564-.51.158-.592.525-.915 1.074-.915.61 0 1.03.446 1.03 1.084 0 .563-.208.885-.822 1.325-.619.433-.926.914-.926 1.64v.111c0 .428.208.745.585.745" />
@@ -116,16 +142,16 @@
                                                 <path d="M7.001 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0" />
                                             </svg>
 
-                                            FAQ</a></li>
-                                    <li class="support-item-footer caption-1"><a href="contact-us.php"
-                                            class="text-body"><svg xmlns="http://www.w3.org/2000/svg" width="16"
-                                                height="16" fill="currentColor" class="bi bi-telephone"
+                                            &nbsp;FAQ</a></li>
+                                    <li class="support-item-footer caption-1" style="font-size: 16px !important; font-weight: normal !important;"><a href="contact-us.php"
+                                            class="text-body"><svg xmlns="http://www.w3.org/2000/svg" width="22"
+                                                height="22" fill="currentColor" class="bi bi-telephone"
                                                 viewBox="0 0 16 16">
                                                 <path
                                                     d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
                                             </svg>
 
-                                            Contact</a>
+                                            &nbsp;Contact</a>
                                     </li>
                                 </ul>
                             </div>
@@ -133,7 +159,7 @@
                     </div>
                     <div class="footer-right">
                         <div class="footer-subscribe">
-                            <div class="label text-btn-uppercase text-body">
+                            <div class="label text-btn-uppercase text-body" style="font-size: 16px !important; font-weight: bold !important;">
                                 Subscribe for all the top news!
                             </div>
                             <fieldset>
@@ -154,7 +180,7 @@
                                     <a href="https://www.linkedin.com/company/impact-nest">
                                         <div class="" style="width: 30px !important;">
                                             <!-- <i class="icon-linkedin"></i> -->
-                                             <img src="image/icon/Linkedin-Black.png" alt="linkedin">
+                                            <img src="image/icon/Linkedin-Black.png" alt="linkedin">
                                         </div>
                                     </a>
                                 </li>
